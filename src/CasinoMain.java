cf380b6eaec8fa0684e32575a89755ee8118bd00
MatteoPaganini <matteo_paganini25@milton.edu>
2024-05-13 15:19:43 -0400

    adding a counter

--- src/CasinoMain.java
+++ src/CasinoMain.java
@@ -10,16 +10,25 @@ public class CasinoMain {
         deck = new Card[52];
         //fill
 
+        int counter = 0;
         for (int f = 1; f <= 4; f = f+1) {
             for (int i = 1; i <= 13; i++) {
                 Card c = new Card(i, f);
-                deck[i - 1] = c;
-                deck[i - 1].printCard();
+                deck[counter] = c;
+                counter ++;
+//                deck[i - 1].printCard();
             }
         }
-
-
-
-
+        printDeck();
     }
+
+    public void printDeck(){
+        //for loop to loop through all cards
+        //for each card
+        for (int i = 0; i <= 52; i++){
+            deck[i].printCard();
+        }
+    }
+
+
 }
